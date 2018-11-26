db8390ca42bc9ca8c40a290806c32bbcc716afb5
Christopher Neuwirth <christopher.neuwirth@mhp.com>
2018-11-26 11:10:12 +0100

    Forked Changes - Made fnKeys array make use of all available 24 keys provided by iterm touchbar - default yarn false - added empty behavior if no package.json available - make use of emojis - changed README to match the fork

--- README.md
+++ README.md
@@ -2,22 +2,22 @@
 
 > Displays package.json scripts in touchbar, a fork of [iam4x touchbar](https://github.com/iam4x/zsh-iterm-touchbar).
 
-*WARNING!* You must not have any keys binded to function in order for this script to work as it overwrites F1-F12.
+_WARNING!_ You must not have any keys binded to function in order for this script to work as it overwrites F1-F24.
 
 ![preview1](./ss.png)
 ![preview2](./ss2.png)
 
 ### Requirements
 
-* iTerm2 3.1.beta.3 (OS 10.10+) - [Download](https://www.iterm2.com/downloads.html)
-* [zsh](http://www.zsh.org/) shell
+- iTerm2 3.1.beta.3 (OS 10.10+) - [Download](https://www.iterm2.com/downloads.html)
+- [zsh](http://www.zsh.org/) shell
 
 ### Installing plugin
 
 Clone the repo in your plugins directory:
 
-* `$ cd ${ZSH_CUSTOM1:-$ZSH/custom}/plugins`
-* `$ git clone https://github.com/seripap/pkgjson-iterm-touchbar.git`
+- `$ cd ${ZSH_CUSTOM1:-$ZSH/custom}/plugins`
+- `$ git clone https://github.com/ChristopherNeuwirth/pkgjson-iterm-touchbar`
 
 Then add the plugin into your `~/.zshrc`:
 
@@ -27,4 +27,4 @@ plugins=(... pkgjson-iterm-touchbar)
 
 ### Configuring for NPM instead of yarn
 
-In `pkgjson-iterm-touchbar.plugin.zsh`, modify useYarn to `false` on line 6.
+In `pkgjson-iterm-touchbar.plugin.zsh`, modify useYarn to `true` on line 6 if you want to use it instead of npm.
--- pkgjson-iterm-touchbar.plugin.zsh
+++ pkgjson-iterm-touchbar.plugin.zsh
@@ -1,12 +1,13 @@
 # F1-12: https://github.com/vmalloc/zsh-config/blob/master/extras/function_keys.zsh
-fnKeys=('^[OP' '^[OQ' '^[OR' '^[OS' '^[[15~' '^[[17~' '^[[18~' '^[[19~' '^[[20~' '^[[21~' '^[[23~' '^[[24~')
+fnKeys=('^[OP' '^[OQ' '^[OR' '^[OS' '^[[15~' '^[[17~' '^[[18~' '^[[19~' '^[[20~' '^[[21~' '^[[23~' '^[[24~', '^[[25~', '^[[26~', '^[[27~', '^[[28~', '^[[29~', '^[[30~', '^[[31~', '^[[32~', '^[[33~', '^[[34~', '^[[35~', '^[[36~')
 touchBarState=''
 npmScripts=()
 lastPackageJsonPath=''
-useYarn=true
+useYarn=false
 
 function _clearTouchbar() {
-  echo -ne "\033]1337;PopKeyLabels\a"
+  # echo -ne "\033]1337;PopKeyLabels\a"
+  echo -ne "\033]1337;SetKeyLabel=F1=           No package.json in 🗂                                                 \a"
 }
 
 function _unbindTouchbar() {
@@ -32,7 +33,7 @@ function _displayNpmScripts() {
   # find available npm run scripts only if new directory
   if [[ $lastPackageJsonPath != $(echo "$(pwd)/package.json") ]]; then
     lastPackageJsonPath=$(echo "$(pwd)/package.json")
-    npmScripts=($(node -e "console.log(Object.keys($(npm run --json)).filter((name, idx) => idx < 12).join(' '))")) 
+    npmScripts=($(node -e "console.log(Object.keys($(npm run --json)).filter((name, idx) => idx < 24).join(' '))")) 
   fi
 
   _clearTouchbar
@@ -47,7 +48,12 @@ function _displayNpmScripts() {
     else
       bindkey -s $fnKeys[$fnKeysIndex] "npm run $npmScript \n"
     fi
-    echo -ne "\033]1337;SetKeyLabel=F$fnKeysIndex=$npmScript\a"
+    echo -ne "\033]1337;SetKeyLabel=F$fnKeysIndex=👉🏻 $npmScript\a"
+    fnKeysIndex=$((fnKeysIndex + 1))
+  done
+
+  for fnKey in "$fnKeys[@]"; do
+    echo -ne "\033]1337;SetKeyLabel=F$fnKeysIndex=✕\a"
     fnKeysIndex=$((fnKeysIndex + 1))
   done
 }
